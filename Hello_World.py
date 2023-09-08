b69ac89b8f3ebe558f0477a99db93f0ef75ee214
Infinite Gears <144358189+infinite-gears@users.noreply.github.com>
2023-09-08 23:49:51 +0900

    Create Hello_World.py

---
+++ Hello_World.py
@@ -0,0 +1,8 @@
+# Read a full line of input from stdin and save it to our dynamically typed variable, input_string.
+input_string = input()
+
+# Print a string literal saying "Hello, World." to stdout.
+print('Hello, World.')
+
+# Print the contents of the variable on the second line
+print(input_string)
