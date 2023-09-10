d2b9559aabc93fd211dd9ce304637e4b794423fd
Infinite Gears <144358189+infinite-gears@users.noreply.github.com>
2023-09-10 21:33:21 +0900

    Create voting_eligibility_checker.py

---
+++ voting_eligibility_checker.py
@@ -0,0 +1,10 @@
+# Introduction to Conditional Statements
+
+# Get user input for age
+age = int(input("Enter your age: "))
+
+# Check if the user is eligible to vote
+if age >= 18:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote yet.")
