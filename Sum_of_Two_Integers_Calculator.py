555b94708e8e4c85b1db7a3ed36606cdc295af53
Infinite Gears <144358189+infinite-gears@users.noreply.github.com>
2023-09-20 23:32:32 +0900

    Create Sum_of_Two_Integers_Calculator.py

---
+++ Sum_of_Two_Integers_Calculator.py
@@ -0,0 +1,13 @@
+def solveMeFirst(a, b):
+    # Calculate the sum of a and b
+    return a + b
+
+# Read the input values
+num1 = int(input())
+num2 = int(input())
+
+# Calculate the sum using the solveMeFirst function
+res = solveMeFirst(num1, num2)
+
+# Print the result
+print(res)
